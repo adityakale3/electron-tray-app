74ebe50910dd6c26c0222ff0dd5fb75d372830d9
Aditya <adityakale3@gmail.com>
2020-07-03 01:18:39 +0530

    Tray with basic functionality added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app , BrowserWindow }  = require('electron');
+const { app , BrowserWindow, Tray }  = require('electron');
 
 let win;
+let tray;
 
 app.on('ready', () => {
     win = new BrowserWindow({
@@ -8,11 +9,23 @@ app.on('ready', () => {
         height:600,
         webPreferences : {
             nodeIntegration : true
-        }
+        },
+        frame:false,
+        resizable:false,
+        skipTaskbar:true
     });
 
     win.loadFile('index.html');
+    tray = new Tray('music-icon.png');
 
+
+    tray.on('click', () => {
+        if(win.isVisible()){
+            win.hide()
+        }else{
+            win.show()
+        }
+    })
 });
 
 
